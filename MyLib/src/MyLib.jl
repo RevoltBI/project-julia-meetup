5e05d0393f69450e88f9112239c97569a70fbe2c
František Navrkal <frantisek@revolt.bi>
2018-10-22 19:04:54 +0200

    Added lib precompilation demo

---
+++ MyLib/src/MyLib.jl
@@ -0,0 +1,9 @@
+module MyLib
+
+__precompile__(false)
+
+greet() = print("Hello World!")
+
+add(n::Int, m::Int) = n + m
+
+end # module
---
+++ MyLibPrecomp/src/MyLibPrecomp.jl
@@ -0,0 +1,7 @@
+module MyLibPrecomp
+
+greet() = print("Hello World!")
+
+add(n::Int, m::Int) = n + m
+
+end # module
